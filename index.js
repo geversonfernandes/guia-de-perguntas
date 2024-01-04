a9f3a8c69fc274218ca4238140307b490ea4ec94
geversonfernandes <gever.fernandes@gmail.com>
2024-01-04 01:28:39 -0300

    feat: projet created

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+res.render('index')
+})
+
+app.listen(8080, () => {
+    console.log('App running...')
+})	
